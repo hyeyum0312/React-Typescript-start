a143e69bbc9d41fcf56b3af87205ca7be68d263e
조혜연 <hy_312@mncnext.co.kr>
2023-03-17 23:06:35 +0900

    Add: api 호출에러 수정

--- client/src/api/login.ts
+++ client/src/api/login.ts
@@ -42,6 +42,7 @@ export const getCurrentUserInfo = async (): Promise<User | null> => {
 export const getItems = async (): Promise<Item[] | null> => {
   const itemRes = await fetch(`${ BASE_URL }/items`, {
     method: 'GET',
+    credentials:'include', // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
     headers: {
       'Content-Type': 'application/json',
       credentials: 'include'
@@ -55,6 +56,7 @@ export const getItems = async (): Promise<Item[] | null> => {
 export const getAllItems = async (): Promise<Item[] | null> => {
   const itemRes = await fetch(`${ BASE_URL }/all-items`, {
     method: 'GET',
+    credentials:'include', // 클라이언트와 서버가 통신할때 쿠키 값을 공유하겠다는 설정
     headers: {
       'Content-Type': 'application/json',
       credentials: 'include'
